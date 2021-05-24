8e9861ee0376d63d642efe960a0f81e34e7d5b80
shivam270899 <68773657+shivam270899@users.noreply.github.com>
2021-05-24 21:51:37 +0530

    Add files via upload

---
+++ store.js
@@ -0,0 +1,36 @@
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
+import thunk from 'redux-thunk';
+import { cartReducer } from './reducers/cartReducers';
+import { orderCreateReducer, orderDetailsReducer, orderMineListReducer, orderPayReducer } from './reducers/orderReducer';
+import { productDetailsReducer, productListReducer } from './reducers/productReducers';
+import { userDetailsReducers, userRegisterReducer, userSigninReducer, userUpdateProfileReducer } from './reducers/userReducer';
+
+
+const initialstate = {
+    userSignin: {
+        userInfo: localStorage.getItem('userInfo')? JSON.parse(localStorage.getItem('userInfo')):null,
+    },
+    cart: {
+        cartItems: localStorage.getItem('cartItems')? JSON.parse(localStorage.getItem('cartItems')): [],
+        shippingAddress: localStorage.getItem('shippingAddress')? JSON.parse(localStorage.getItem('shippingAddress')): {},
+        paymentMethod: 'PayPal'
+    }
+};
+const reducer = combineReducers({
+    productList : productListReducer,
+    productDetails: productDetailsReducer,
+    cart: cartReducer,
+    userSignin: userSigninReducer,
+    userRegister: userRegisterReducer,
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
+    orderPay: orderPayReducer,
+    orderMineList: orderMineListReducer,
+    userDetails: userDetailsReducers,
+    userUpdateProfile: userUpdateProfileReducer,
+})
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialstate, composeEnhancer(applyMiddleware(thunk)));
+
+export default store;
